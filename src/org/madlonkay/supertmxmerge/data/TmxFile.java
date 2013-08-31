78508ca1a4fd5893ad224da6555aff9a650f280c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2013-08-31 13:13:55 +0900

    Make system DTDs resolve when unmarshaling TMX files

--- src/org/madlonkay/supertmxmerge/data/TmxFile.java
+++ src/org/madlonkay/supertmxmerge/data/TmxFile.java
@@ -9,6 +9,7 @@ import gen.core.tmx14.Tuv;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,8 +17,17 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.UnmarshalException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
 import org.madlonkay.supertmxmerge.SuperTmxMerge;
 import org.madlonkay.supertmxmerge.util.TuvUtil;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
  *
@@ -25,27 +35,54 @@ import org.madlonkay.supertmxmerge.util.TuvUtil;
  */
 public class TmxFile {
 
-    private static Unmarshaller u = null;
+    private static Unmarshaller UNMARSHALLER = null;
+    /**
+     * Special XMLReader with EntityResolver required to resolve system DTD per
+     * http://stackoverflow.com/questions/3587196/jaxb-saxparseexception-when-unmarshalling-document-with-relative-path-to-dtd
+     */
+    private static XMLReader XMLREADER = null;
     private Tmx doc = null;
     private String filepath = null;
     private Map<String, Tuv> anonymousTuvs = null;
+    
+    private static final String FEATURE_NAMESPACES = "http://xml.org/sax/features/namespaces";
+    private static final String FEATURE_NAMESPACE_PREFIXES = "http://xml.org/sax/features/namespace-prefixes";
 
     static {
         try {
-            JAXBContext jc = JAXBContext.newInstance(Tmx.class.getPackage().getName());
-            u = jc.createUnmarshaller();
+            JAXBContext jc = JAXBContext.newInstance(Tmx.class);
+            UNMARSHALLER = jc.createUnmarshaller();
+            XMLReader xmlreader = XMLReaderFactory.createXMLReader();
+            xmlreader.setFeature(FEATURE_NAMESPACES, true);
+            xmlreader.setFeature(FEATURE_NAMESPACE_PREFIXES, true);
+            xmlreader.setEntityResolver(new EntityResolver() {
+                @Override
+                public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                    File systemFile = new File(systemId);
+                    InputSource s = new InputSource(TmxFile.class.getResourceAsStream("/schemas/" + systemFile.getName()));
+                    return s;
+                }
+            });
+            XMLREADER = xmlreader;
         } catch (JAXBException ex) {
             Logger.getLogger(SuperTmxMerge.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXNotRecognizedException ex) {
+            Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXNotSupportedException ex) {
+            Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public TmxFile(String filepath) throws UnmarshalException {
         this.filepath = filepath;
         try {
-            this.doc = (Tmx) u.unmarshal(new FileInputStream(filepath));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
+            Source source = new SAXSource(XMLREADER, new InputSource(new FileInputStream(filepath)));
+            this.doc = (Tmx) UNMARSHALLER.unmarshal(source);
         } catch (JAXBException ex) {
+            Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(TmxFile.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
